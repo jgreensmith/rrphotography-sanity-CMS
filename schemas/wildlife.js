cd26af0aa97466c5cd7c99b8509faee9bf18487a
James Greensmith <jgreensmith13@gmail.com>
2021-09-05 20:38:43 +0100

    fixed schema

--- schemas/wildlife.js
+++ schemas/wildlife.js
@@ -12,17 +12,12 @@ export default {
             }, 
         },
         {
-            name: 'caption',
+            name: 'alt',
             type: 'string',
             title: 'Caption',
             description: 'tell us about your piccy!',
         },
-        {
-            name: 'alt',
-            type: 'string',
-            title: 'Alternative Text',
-            description: 'important for SEO, example: Puffin',
-        },
+        
 
     ]
 }
